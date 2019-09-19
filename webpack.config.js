626a79aaf078ae231312ab1f35a2986597bdcd15
Kary Ma <karyma101@hotmail.com>
2019-09-19 14:53:52 -0700

    basic react setup

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html"
+})
+
+module.exports = {
+
+  output: {
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /(\.global\.css$|react-select.css)/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /^((?!\.global|react-select).)*\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        loader: 'file-loader'
+      },
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [htmlWebpackPlugin]
+}
